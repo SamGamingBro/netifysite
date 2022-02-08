673c42809097c05757c3caa8d597a7797cc005ad
JoSamYT_HD <51163942+SamGamingBro@users.noreply.github.com>
2022-02-08 17:53:56 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          ISucht Service sind nicht verfügbar! <code>pages/index.js</code>
         </p>
       </main>
 
